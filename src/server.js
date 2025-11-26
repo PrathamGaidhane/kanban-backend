6cfd02096569e8d3cede096b22d48ee6163955a4
PrathamGaidhane <prathamgaidhane123321@gmail.com>
2025-11-27 00:36:11 +0530

    fix: proper CORS setup

--- src/server.js
+++ src/server.js
@@ -7,8 +7,17 @@ import { connectDB } from "./config/db.js";
 dotenv.config();
 const app = express();
 
-// Middlewares
-app.use(cors());
+// ======================
+// FIXED CORS (WORKING)
+// ======================
+app.use(cors({
+  origin: [
+    "https://kanban-board-git-main-pratham-gaidhanes-projects.vercel.app",
+    "http://localhost:5173"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
+
 app.use(express.json());
 
 // DB Connection
@@ -20,8 +29,3 @@ app.use("/api/tasks", taskRoutes);
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
-
-app.use(cors({
-  origin: "*",
-  methods: "GET,POST,PUT,DELETE"
-}));
